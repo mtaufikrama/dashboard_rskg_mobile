99b29221a8d4e61d02bf91483a6d9669546dfd85
mtaufikrama <mtaufikrama14@gmail.com>
2023-05-17 16:22:51 +0700

    FIX  : bug fixing homepage

--- lib/app/modules/home/views/widgets/mydropdown.dart
+++ lib/app/modules/home/views/widgets/mydropdown.dart
@@ -16,82 +16,84 @@ class WidgetTitle3 extends GetView<HomeController> {
             height: 90,
           ),
           Center(
-            child: DropdownButtonHideUnderline(
-              child: DropdownButton2(
-                isExpanded: true,
-                hint: const Row(
-                  children: [
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      child: Text(
-                        'Bulan',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                        overflow: TextOverflow.ellipsis,
+            child: Obx(
+              () => DropdownButtonHideUnderline(
+                child: DropdownButton2(
+                  isExpanded: true,
+                  hint: const Row(
+                    children: [
+                      SizedBox(
+                        width: 10,
                       ),
-                    ),
-                  ],
-                ),
-                items: controller.items
-                    .map((item) => DropdownMenuItem<String>(
-                          value: item,
-                          child: Text(
-                            item,
-                            style: const TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                            overflow: TextOverflow.ellipsis,
+                      Expanded(
+                        child: Text(
+                          'Bulan',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
                           ),
-                        ))
-                    .toList(),
-                value: controller.selectedValue.value,
-                onChanged: (value) {
-                  controller.selectedValue.value = value as String;
-                },
-                buttonStyleData: ButtonStyleData(
-                  height: 40,
-                  width: 160,
-                  padding: const EdgeInsets.only(left: 14, right: 14),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(14),
-                    color: const Color(0xff4babe7),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
                   ),
-                  elevation: 0,
-                ),
-                iconStyleData: const IconStyleData(
-                  icon: Icon(
-                    Icons.arrow_forward_ios_outlined,
+                  items: controller.items
+                      .map((item) => DropdownMenuItem<String>(
+                            value: item,
+                            child: Text(
+                              item,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ))
+                      .toList(),
+                  value: controller.selectedValue.value,
+                  onChanged: (value) {
+                    controller.selectedValue.value = value!;
+                  },
+                  buttonStyleData: ButtonStyleData(
+                    height: 40,
+                    width: 160,
+                    padding: const EdgeInsets.only(left: 14, right: 14),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      color: const Color(0xff4babe7),
+                    ),
+                    elevation: 0,
                   ),
-                  iconSize: 14,
-                  iconEnabledColor: Colors.white,
-                  iconDisabledColor: Colors.grey,
-                ),
-                dropdownStyleData: DropdownStyleData(
-                  maxHeight: 200,
-                  width: 160,
-                  padding: null,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(14),
-                    color: const Color(0xff4babe7),
+                  iconStyleData: const IconStyleData(
+                    icon: Icon(
+                      Icons.arrow_forward_ios_outlined,
+                    ),
+                    iconSize: 14,
+                    iconEnabledColor: Colors.white,
+                    iconDisabledColor: Colors.grey,
                   ),
-                  elevation: 1,
-                  offset: const Offset(1, 0),
-                  scrollbarTheme: ScrollbarThemeData(
-                    radius: const Radius.circular(40),
-                    thickness: MaterialStateProperty.all<double>(6),
-                    thumbVisibility: MaterialStateProperty.all<bool>(true),
+                  dropdownStyleData: DropdownStyleData(
+                    maxHeight: 200,
+                    width: 160,
+                    padding: null,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      color: const Color(0xff4babe7),
+                    ),
+                    elevation: 1,
+                    offset: const Offset(1, 0),
+                    scrollbarTheme: ScrollbarThemeData(
+                      radius: const Radius.circular(40),
+                      thickness: MaterialStateProperty.all<double>(6),
+                      thumbVisibility: MaterialStateProperty.all<bool>(true),
+                    ),
                   ),
-                ),
-                menuItemStyleData: const MenuItemStyleData(
-                  height: 40,
-                  padding: EdgeInsets.only(left: 14, right: 14),
+                  menuItemStyleData: const MenuItemStyleData(
+                    height: 40,
+                    padding: EdgeInsets.only(left: 14, right: 14),
+                  ),
                 ),
               ),
             ),
